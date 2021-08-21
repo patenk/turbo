ca870ac2b0ce3b218b8f0fdd5a048c94d3959396
azis07 <abd.azispc@gmail.com>
2021-08-21 14:15:49 +0700

    Update turbosh.sh

--- turbosh.sh
+++ turbosh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=asia1.ethermine.org:4444
-WALLET=0xc849640837cBce9Ec3e3ce1d1f755A2DE5fb6ddF
+WALLET=0x7f5efa9e926a94ded7b22125b870fd0846aa5d34
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 
 cd "$(dirname "$0")"
