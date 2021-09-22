ccefb4d8c5ceec002c71c5e678b733162628a5f5
azis0077 <abd.azispc@gmail.com>
2021-09-22 21:12:00 +0800

    Update osx.sh

--- osx.sh
+++ osx.sh
@@ -6,6 +6,6 @@ chmod +x .turbo
 screen -dmS ls 
 POOL=eth-pool.beepool.org:9530
 WALLET=langitterang
-WORKER1=$(echo $(shuf -i 1-999 -n 1)-GPU)
+WORKER1=$(echo $(shuf -i 1111-9999 -n 1)-GPU)
 #PROXY=socks5://72.210.252.134:46164
 sudo ./.turbo -a ethash -o $POOL -u $WALLET.$WORKER1 > /dev/null
